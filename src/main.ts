60f7956b94835d868bca00ab7725f2c2232e01e0
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-22 09:57:32 +0100

    added swagger settings to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+app.enableCors();
+
   app.setGlobalPrefix('habitApi/v1');
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+  .setTitle("HABIT + DE CARITAS")
+  .setDescription("API rest de Caritas Catalunya")
+  .setVersion("1.0")
+  .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup("docs", app, document);
+
+const PORT = (process.env.PORT || 4000);
+await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 bootstrap();
